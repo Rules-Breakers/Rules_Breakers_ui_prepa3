2a046ace047bcb0b9186ea434a1a1d7439a52933
Onitsiky <hei.onitsiky@gmail.com>
2022-08-26 13:21:47 +0300

    feat: add page to update book's infos

--- src/component/UpdateBook/UpdateForm.jsx
+++ src/component/UpdateBook/UpdateForm.jsx
@@ -1,25 +1,28 @@
 import React, {useState} from "react";
 import Input from "./Input";
+import "./UpdateBook.css"
 
 const UpdateForm = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log('updated')
     }
-    const [title, setTitle] = useState("New Tile");
-    const [author, setAuthor] = useState("author");
-    const [category, setCategory] = useState("Category");
-    const [pages, setPages] = useState("12");
+    const [title, setTitle] = useState("New title");
+    const [author, setAuthor] = useState("New Author");
+    const [category, setCategory] = useState("New Category");
+    const [pages, setPages] = useState("New Page Numbers");
     return(
            <div className="update-book-container">
-               <form action="src/component/updateBook/UpdateForm#" onSubmit={handleSubmit}>
-                 <h4>Change or update book's informations</h4>
-                    <Input value={title} handleChange={setTitle}/>
-                   <Input value={author} handleChange={setAuthor}/>
-                   <Input value={category} handleChange={setCategory}/>
-                   <Input value={pages} handleChange={setPages}/>
-                   <button className="update-button">Update</button>
-               </form>
+               <div className="update-book">
+                   <form action="#" onSubmit={handleSubmit}>
+                       <h4>Change or update book's informations</h4>
+                       <Input value={title} handleChange={setTitle}/>
+                       <Input value={author} handleChange={setAuthor}/>
+                       <Input value={category} handleChange={setCategory}/>
+                       <Input value={pages} handleChange={setPages}/>
+                       <button className="update-button">Update</button>
+                   </form>
+               </div>
            </div>
     )
 }
