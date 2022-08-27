93ee7a9ecf749de40336be55163a2b5b0cbc4bed
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2022-08-27 10:37:05 +0300

    'commityyyyyy'

--- src/component/History/Historique.jsx
+++ src/component/History/Historique.jsx
@@ -5,25 +5,50 @@ import '../bootstrap/bootstrap.css';
 function Historique(){
     return (
         <>
-          <div className="Container2">
-            <div className="Containercontainer2">
-              <div className="before">
-                <p className="ttle">Title - Auteur</p>
-                <h3>Historique des emprunts</h3>
-              </div>
-             <div className="Timing">
-                <p className="time">2022-04-12 10h30</p>
-                <p className="time">2022-04-13 10h20</p>
-                <p className="time">2022-04-12 10h30</p>
-                <p className="time">2022-04-13 10h20</p>
-                <p className="time">2022-04-12 10h30</p>
-                <p className="time">2022-04-13 10h20</p>
-                <p className="time">2022-04-12 10h30</p>
-                <p className="time">2022-04-13 10h20</p>
-             </div>
-          </div>
+        <div className="contain">
+        <h2 className="history text-center">Historique des emprunts</h2>
+          <div className="Containing">
+            <div className="left">
+              <p className="ttle">Title</p>
+              <p className="ttle">Auteur</p>
             </div>
-             
+            <div className="right">
+              <table>
+                <tbody>
+                    <tr>
+                        <td className="p-2 tab">2022-06-11</td>
+                        <td className="p-2 tab">20h30</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 tab">2022-06-11</td>
+                        <td className="p-2 tab">20h30</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 tab">2022-06-11</td>
+                        <td className="p-2 tab">20h30</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 tab">2022-06-11</td>
+                        <td className="p-2 tab">20h30</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 tab">2022-06-11</td>
+                        <td className="p-2 tab">20h30</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 tab">2022-06-11</td>
+                        <td className="p-2 tab">20h30</td>
+                    </tr>
+                    <tr>
+                        <td className="p-2 tab">2022-06-11</td>
+                        <td className="p-2 tab">20h30</td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+          </div>   
+        </div>
+          
         </>
     )
 }
